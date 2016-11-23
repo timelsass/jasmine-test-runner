d362a76057ddea4608f783589ae659b3916355c6
Tim <dev@tim.ph>
2016-11-23 02:12:37 -0500

    updating inline js doc blocks

--- assets/js/jasmine-test-runner.js
+++ assets/js/jasmine-test-runner.js
@@ -5,9 +5,30 @@ var JasmineTestRunner = JasmineTestRunner || {};
 	'use strict';
 
 	JasmineTestRunner = {
+
+		/**
+		 * Responsible for initilization of JasmineTestRunner.
+		 *
+		 * @since 0.1
+		 *
+		 * @returns null Nothing returned.
+		 */
 		init: function() {
 			$( window ).on( 'jasmineToggled', this.hideToggle );
 		},
+
+		/**
+		 * Hides/Shows the Jasmine Test Runner.
+		 *
+		 * This method hides and shows the Jasmine Test Runner
+		 * content from the toolbar.  The icon is triggered by
+		 * the click that occurs on the arrow in the upper right
+		 * hand corner of the Jasmine Test Runner toolbar.
+		 *
+		 * @since 0.1
+		 *
+		 * @returns null Nothing returned.
+		 */
 		hideToggle: function() {
 			var arrow =  $( '.jasmine-hide-toggle' );
 			var selectors = $( '.jasmine-symbol-summary, .jasmine-alert, .jasmine-status, .jasmine-results' );
